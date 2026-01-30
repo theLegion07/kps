5ffac183f3ae6398c1f2d96105f3a1717f0d3c84
theLegion07 <pranaysaikolloju.kps07@gmail.com>
2026-01-30 20:30:02 +0530

    splash Screen Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import AppRoutes from "./AppRoutes"
 import { HashRouter as Router } from "react-router-dom"
-// import Splash from "./Components/Subs/Splash"
+import Splash from "./Components/Subs/Splash"
 
 function App() {
-  // const [splash,setsplash] = useState<boolean>(true)
-  // useEffect(() => {
-  //  setTimeout(() => {
-  //   setsplash(false)
-  //  }, 2000);
-  // },[])
+  const [splash,setsplash] = useState<boolean>(true)
+  useEffect(() => {
+   setTimeout(() => {
+    setsplash(false)
+   }, 2000);
+  },[])
   return (
     <>
-      {/* {splash ? <Splash /> : ( */}
+      {splash ? <Splash OnFinish={() => setsplash(false)} /> : (
           <Router >
        <AppRoutes />
      </Router>
-      {/* )} */}
+     )} 
      
     
     </>
--- src/Components/Subs/Splash.tsx
+++ src/Components/Subs/Splash.tsx
@@ -1,16 +1,43 @@
-import { motion } from "framer-motion"
+import splashlogo from '../../assets/Kps.svg'
+import { useEffect, useState } from "react"
 
-const Splash = () => {
+type finish = {
+  OnFinish : () => void ;
+}
+const Splash : React.FC<finish> = ({OnFinish}) => {
+  const [progress,setprogress] = useState(0);
+   useEffect(() => {
+    const interval = setInterval(() => {
+      setprogress(prev => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          OnFinish(); 
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 40); 
+
+    return () => clearInterval(interval);
+  }, [OnFinish]);
   return (
-    <div className='min-h-screen text-white bg-black flex justify-center items-center'>
-      <motion.div
-      >
-        <div className="bg-white max-w-100 aspect-square">
-          a
-        </div>
-      </motion.div>
+    <div className='min-h-screen text-white bg-black flex flex-col justify-center items-center'>
+      <div>
+        <div>
+         <img src={splashlogo} alt="entry-logo" className="w-13 sm:w-15 aspect-square" />
+      </div>
+      <div >
+         <div  className={`h-1 rounded-2xl w-full bg-black`} style={{...styles.fill,width:`${progress}%`}} />
+      </div>
+      </div>
+      
     </div>
   )
 }
-
+const styles = {
+  fill:{
+    background:'yellow',
+    transition : 'width 0.2s',
+  }
+}
 export default Splash
